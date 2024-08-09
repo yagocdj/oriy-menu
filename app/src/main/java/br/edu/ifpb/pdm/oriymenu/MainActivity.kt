4e48bf50e5cded78f3d372f0acf5bc4a16b446e2
Yago César <yagocdj@gmail.com>
2024-08-08 21:32:12 -0300

    creating a new composable called MainApp that contains the app's scaffold (and, consequently, the app's content)

--- app/src/main/java/br/edu/ifpb/pdm/oriymenu/MainActivity.kt
+++ app/src/main/java/br/edu/ifpb/pdm/oriymenu/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,93 +26,63 @@ import br.edu.ifpb.pdm.oriymenu.ui.theme.telas.RegisterForm
 
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             OriymenuTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            colors = topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            title = {
-                                Text("ORYI Menu")
-                            },
-                            modifier = Modifier.padding(bottom = 16.dp)
-                        )
-                    }, modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "login") {
-                        composable("login") {
-                            LoginScreen(onSignInClick = {
-                                navController.navigate("home")
-                            }, onRegisterClick = {
-                                navController.navigate("register")
-                            })
-                        }
-                        composable("home") {
-                            HomeScreen(modifier = Modifier.padding(innerPadding), onLogoffClick = {
-                                navController.navigate("login")
-                            })
-                        }
-                        composable("register") {
-                            RegisterForm(
-                                modifier = Modifier.padding(innerPadding),
-                                onRegisterSuccessClick = {
-                                    navController.navigate("home")
-                                })
-                        }
-                    }
-                }
+                MainApp()
             }
         }
     }
 }
-//
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    OriymenuTheme {
-//        Scaffold(
-//            topBar = {
-//                TopAppBar(
-//                    colors = topAppBarColors(
-//                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-//                        titleContentColor = MaterialTheme.colorScheme.primary,
-//                    ),
-//                    title = {
-//                        Text("Navegação")
-//                    },
-//                    modifier = Modifier.padding(bottom = 16.dp)
-//                )
-//            }, modifier = Modifier.fillMaxSize()) { innerPadding ->
-//            val navController = rememberNavController()
-//            NavHost(navController = navController, startDestination = "login") {
-//                composable("login") {
-//                    LoginScreen(onSignInClick = {
-//                        navController.navigate("home")
-//                    }, onRegisterClick = {
-//                        navController.navigate("register")
-//                    })
-//                }
-//                composable("home") {
-//                    HomeScreen(modifier = Modifier.padding(innerPadding), onLogoffClick = {
-//                        navController.navigate("login")
-//                    })
-//                }
-//                composable("register") {
-//                    RegisterScreen(modifier = Modifier.padding(innerPadding), onRegisterSuccessClick = {
-//                        navController.navigate("home")
-//                    })
-//                }
-//            }
-//        }
-//    }
-//}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainApp() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("ORYI Menu")
+                },
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }, modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "login") {
+            composable("login") {
+                LoginScreen(onSignInClick = {
+                    navController.navigate("home")
+                }, onRegisterClick = {
+                    navController.navigate("register")
+                })
+            }
+            composable("home") {
+                HomeScreen(modifier = Modifier.padding(innerPadding), onLogoffClick = {
+                    navController.navigate("login")
+                })
+            }
+            composable("register") {
+                RegisterForm(
+                    modifier = Modifier.padding(innerPadding),
+                    onRegisterSuccessClick = {
+                        navController.navigate("home")
+                    })
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainAppPreview() {
+    OriymenuTheme {
+        MainApp()
+    }
+}

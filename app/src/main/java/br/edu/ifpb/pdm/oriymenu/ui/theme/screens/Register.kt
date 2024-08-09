df31249e0f4535d96ffb320925110a4abd15dfec
Yago César <yagocdj@gmail.com>
2024-08-08 22:00:14 -0300

    change RegisterForm's title alignment

--- app/src/main/java/br/edu/ifpb/pdm/oriymenu/ui/theme/screens/Register.kt
+++ app/src/main/java/br/edu/ifpb/pdm/oriymenu/ui/theme/screens/Register.kt
@@ -1,8 +1,10 @@
 package br.edu.ifpb.pdm.oriymenu.ui.theme.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -19,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,11 +43,17 @@ fun RegisterForm(modifier: Modifier = Modifier, onRegisterSuccessClick: () -> Un
             .padding(top = 32.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Cadastro",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 50.dp)
+        ) {
+            Text(
+                text = "Cadastro",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start
+            )
+        }
         Spacer(modifier = Modifier.height(6.dp))
         OutlinedTextField(
             value = fullName,
